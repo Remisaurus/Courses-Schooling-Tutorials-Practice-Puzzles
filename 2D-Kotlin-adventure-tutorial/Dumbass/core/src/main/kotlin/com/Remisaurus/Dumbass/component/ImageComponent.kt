d149c77c83d379fd353a1999d140511b159f6649
Remisaurus <remi.meijer@gmail.com>
2024-02-07 11:38:19 -0700

    hmm

---
+++ 2D-Kotlin-adventure-tutorial/Dumbass/core/src/main/kotlin/com/Remisaurus/Dumbass/component/ImageComponent.kt
@@ -0,0 +1,9 @@
+@file:Suppress("PackageName")
+
+package com.Remisaurus.Dumbass.component
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+
+class ImageComponent {
+    lateinit var image: Image
+}
--- 2D-Kotlin-adventure-tutorial/Dumbass/core/src/main/kotlin/com/Remisaurus/Dumbass/screens/GameScreen.kt
+++ 2D-Kotlin-adventure-tutorial/Dumbass/core/src/main/kotlin/com/Remisaurus/Dumbass/screens/GameScreen.kt
@@ -2,17 +2,27 @@
 
 package com.Remisaurus.Dumbass.screens
 
+import com.Remisaurus.Dumbass.systems.RenderSystem
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.utils.Scaling
 import com.badlogic.gdx.utils.viewport.ExtendViewport
+import com.github.quillraven.fleks.World
 import ktx.app.KtxScreen
+import ktx.assets.disposeSafely
 import ktx.log.logger
 
 class GameScreen : KtxScreen {
     private val stage: Stage = Stage(ExtendViewport(16f, 9f ))
     private val texture: Texture = Texture("assets/images/player.png")
+
+    private val world: World = World {
+        inject(stage)
+        system<RenderSystem>()
+
+    }
+
     override fun show() {
         log.debug {"GameScreen get shown"}
         stage.addActor(
@@ -29,15 +39,13 @@ class GameScreen : KtxScreen {
     }
 
     override fun render(delta: Float) {
-        with(stage) {
-            act(delta)
-            draw()
-        }
+        world.update(delta)
     }
 
     override fun dispose() {
-        stage.disposeSafely
+        stage.disposeSafely()
         texture.disposeSafely()
+        world.dispose()
     }
 
     companion object {
