752cb53949d5f3b1d98b6bd3ccbbaa401014d723
Remisaurus <remi.meijer@gmail.com>
2024-03-11 14:40:13 -0600

    11-3-24

--- 2D-Kotlin-adventure-tutorial/Dumbass/core/src/main/kotlin/com/Remisaurus/Dumbass/component/ImageComponent.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("PackageName")
-
-package com.Remisaurus.Dumbass.component
-
-import com.badlogic.gdx.scenes.scene2d.ui.Image
-
-class ImageComponent {
-    lateinit var image: Image
-}
